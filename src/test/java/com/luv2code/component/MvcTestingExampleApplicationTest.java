b6647b121fbf3759bacc204271af67686ee9e8bd
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2024-02-07 22:32:32 +0100

    pom configuration and initial test

---
+++ src/test/java/com/luv2code/component/MvcTestingExampleApplicationTest.java
@@ -0,0 +1,15 @@
+package com.luv2code.component;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+@SpringBootTest(classes = MvcTestingExampleApplication.class)
+class MvcTestingExampleApplicationTest {
+
+    @Test
+    void basicTest(){
+
+    }
+
+}
